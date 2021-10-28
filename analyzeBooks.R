06caeba1c585ccbfaff99cd362b22a12d09a1710
thjkral <thjkral@gmail.com>
2021-10-28 22:48:53 +0200

    Rearranged functions for better readability

--- analyzeBooks.R
+++ analyzeBooks.R
@@ -1,6 +1,12 @@
-setwd("/home/tom/Projects/LotR_Books_vs_movies/BookData")
+# Author: Tom Kral
+# About:  This script is for analyzing a text/book in .txt format. 
+# Manual: The entire script can be called at once to perform the analyses. As per R rules, all functions are generated first.
+#         Operations are called at the bottom of the script.
 
-# Load libraries
+################
+# DEPENDENCIES #
+################
+
 #install.packages("RWeka") # install RWeka package
 #install.packages("readr")
 #install.packages("textdata")
@@ -17,45 +23,25 @@ library(readr)
 library(stringr)
 
 
-# Load data
 
-#fotr <- read_file("01 - The Fellowship Of The Ring.txt")
-#ttt <- read_file("02 - The Two Towers.txt")
-#rotk <- read_file("03 - The Return Of The King.txt")
-fotr <- read_file("test.txt")
+#############
+# FUNCTIONS #
+#############
 
-
-# Print some metrics
-
-printDiversity <- function(text){
+cleanText <- function(text){ # cleans text 
   
-  wordList <- unlist(str_split(text, " "))
-  totalWords <- length(wordList)
-  uniqueWords <- length(unique(wordList))
-  lexDiv <- uniqueWords / totalWords
-  
-  print(cat("Total words: ", totalWords, "\n Unique words: ", uniqueWords, "\n Lexical diversity: ", lexDiv, "\n"))
-  
-}
-printDiversity(fotr)
-
-
-# Functions for cleanng the texts
-
-cleanText <- function(text){
-  
-  text <- str_conv(text, "latin1")
-  text <- gsub("[[\\n\\r]]", "", text)
-  text <- gsub("[[:punct:]]", "", text)
-  text <- gsub("[[:digit:]]", "", text)
-  text <- tolower(text)
-  text <- str_squish(text)
+  text <- str_conv(text, "latin1") # change encoding
+  text <- gsub("[[\\n\\r]]", "", text) # remove \n and \r made by read_fie
+  text <- gsub("[[:punct:]]", "", text) # delete all punctuation
+  text <- gsub("[[:digit:]]", "", text) # remove all numbers
+  text <- tolower(text) # makes all characters lowercase
+  text <- str_squish(text) # removes repeating whitespace
   
   return(text)
   
 }
 
-removeStopwords <- function(text){
+removeStopwords <- function(text){ # removes all English stopwords from a text
   
   textList <- unlist(str_split(text, " "))
   text_nsw <- textList[!(textList) %in% stop_words$word]
@@ -65,15 +51,71 @@ removeStopwords <- function(text){
   
 }
 
+printDiversity <- function(text){ # print the lexical diversity of a text
+  
+  wordList <- unlist(str_split(text, " "))
+  totalWords <- length(wordList)
+  uniqueWords <- length(unique(wordList))
+  lexDiv <- uniqueWords / totalWords
+  
+  print(cat("Total words: ", totalWords, "\n Unique words: ", uniqueWords, "\n Lexical diversity: ", lexDiv, "\n"))
+  
+}
 
-# Clean the texts
 
-fotr_clean <- cleanText(fotr)
-fotr_nsw <- removeStopwords(fotr_clean)
 
-print(fotr_nsw)
+##################################
+# MAIN FUNCTION TO ANAYZE A BOOK #
+##################################
+
+analyzeBook <- function(book){
+  
+  # Print some metrics
+  printDiversity(book)
+  
+  # Clean the texts
+  book_clean <- cleanText(book)
+  book_clean <- removeStopwords(book_clean)
+  
+  print(book_clean)
+  
+  
+}
+
+
+
+#############
+# LOAD DATA #
+#############
+
+
+# Load books as strings
+
+setwd("/home/tom/Projects/LotR_Books_vs_movies/BookData")
+
+#fotr <- read_file("01 - The Fellowship Of The Ring.txt")
+#ttt <- read_file("02 - The Two Towers.txt")
+#rotk <- read_file("03 - The Return Of The King.txt")
+
+# load test file instead
+test <- read_file("test.txt")
+
+test_cleaned <- analyzeBook(test)
 
 
 # Sentiment analysis
+
+performSentimentAnalysis <- function(text){
+  
+  textList <- unlist(str_split(text, " "))
+  df <- data.frame(matrix(unlist(textList), nrow = length(textList), byrow = TRUE), stringsAsFactors = FALSE)
+  
+
+}
+
+fotrTokens <- performSentimentAnalysis(fotr_nsw)
+
+
+
 
 
